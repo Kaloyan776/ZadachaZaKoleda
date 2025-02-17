d706c7373f76091bb89e1d6cf91ec7395840cb53
Kaloyan776 <kpe3779@gmail.com>
2025-02-17 13:46:07 +0200

    Задача за коледа

---
+++ ZadachaZaKoleda/ZAsdfssg/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ZAsdfssg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ ZadachaZaKoleda/ZadachaZaKoleda/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace ZadachaZaKoleda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            while (true)
+            {
+            int num = int.Parse(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        NumPrint();
+                        break;
+                    case 2:
+                        SumOfNums();
+                        break;
+                    case 3:
+                        EvenNums();
+                        break;
+                    case 4:
+                        OddNums();
+                        break;
+                    default:
+                        Console.WriteLine("Грешка! Моля, въведете валидно число от 1 до 4.");
+                        break;
+                }
+                if (num >= 1 && num <=4)
+                {
+                    break;
+                }
+            }
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("                        Меню:");
+            Console.WriteLine();
+            Console.WriteLine("Натиснете 1 за извеждане на числата от 1 до 24.");
+            Console.WriteLine("Натиснете 2 за извеждане на сумата на числата от 1 до 24.");
+            Console.WriteLine("Натиснете 3 за извеждане на четните числа от 1 до 24.");
+            Console.WriteLine("Натиснете 4 за извеждане на нечетните числа от 1 до 24.");
+            Console.WriteLine();
+            Console.Write("Въведете число от 1 до 4: ");
+        }
+
+        static void NumPrint()
+        {
+            for (int i = 1; i <= 23; i++)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.Write("24");
+        }
+
+        static void SumOfNums()
+        {
+            int sum = 0;
+            for(int i = 1;i <= 24; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("Сумата е: " + sum);
+        }
+
+        static void EvenNums()
+        {
+            for (int i = 1; i <= 23; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+                Console.Write("24");
+        }
+
+        static void OddNums()
+        {
+            for (int i = 1; i <= 22; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+                Console.Write("23");
+        }
+    }
+}
